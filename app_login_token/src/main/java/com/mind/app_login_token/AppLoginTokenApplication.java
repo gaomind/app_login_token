ad5ed9cbf833cc9389d7377ea99ab91c7838de53
MIND <mind@MIND-MacBook-Pro.local>
2020-10-09 14:27:41 +0800

    Initial commit

---
+++ app_login_token/src/main/java/com/mind/app_login_token/AppLoginTokenApplication.java
@@ -0,0 +1,13 @@
+package com.mind.app_login_token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLoginTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppLoginTokenApplication.class, args);
+    }
+
+}
---
+++ app_login_token/src/test/java/com/mind/app_login_token/AppLoginTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mind.app_login_token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLoginTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
